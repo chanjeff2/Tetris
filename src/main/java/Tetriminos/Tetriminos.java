0b0c2809fb424e6f25cd41e34611181fd1d2b1c5
chanjeff2 <jeffchan142@gmail.com>
2021-01-19 19:02:17 +0800

    add basic values to Base class Tetriminos

--- src/main/java/Tetriminos/Tetriminos.java
+++ src/main/java/Tetriminos/Tetriminos.java
@@ -1,8 +1,26 @@
 package Tetriminos;
 
 abstract class Tetriminos {
+    public enum Shape {
+        I, J, L, O, S, T, Z
+    }
+
+    Shape shape;
+
+    public enum Orientation {
+        Up, Right, Down, Left
+    }
+
+    Orientation orientation;
+
     public abstract void rotateClockwise();
     public abstract void rotateAntiClockwise();
-    public abstract void drop();
+
+    public abstract void moveLeft();
+    public abstract void moveRight();
+
+    public abstract void softDrop();
+    public abstract void hardDrop();
+
     public abstract void place();
 }
