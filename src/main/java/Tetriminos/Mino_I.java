512040846b479ae0d6c51ebcc6fc48c61745c08f
chanjeff2 <jeffchan142@gmail.com>
2021-01-20 00:03:41 +0800

    basic ui

---
+++ src/main/java/Tetriminos/Mino_I.java
@@ -0,0 +1,57 @@
+package Tetriminos;
+
+import java.util.HashMap;
+
+public class Mino_I extends Tetriminos {
+    public Mino_I(int[] position) {
+        super(position);
+
+        this.type = Type.Mino_I;
+
+        this.shape = new HashMap<Orientation, Byte[][]>();
+
+        this.shape.put(
+            Orientation.Up, 
+            new Byte[][] 
+            {
+                {0, 0, 0, 0},
+                {1, 1, 1, 1},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+            }
+        );
+
+        this.shape.put(
+            Orientation.Right, 
+            new Byte[][] 
+            {
+                {0, 0, 1, 0},
+                {0, 0, 1, 0},
+                {0, 0, 1, 0},
+                {0, 0, 1, 0}
+            }
+        );
+
+        this.shape.put(
+            Orientation.Down, 
+            new Byte[][] 
+            {
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {1, 1, 1, 1},
+                {0, 0, 0, 0}
+            }
+        );
+
+        this.shape.put(
+            Orientation.Left, 
+            new Byte[][] 
+            {
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0},
+                {0, 1, 0, 0}
+            }
+        );
+    }
+}
--- src/main/java/Tetriminos/Tetriminos.java
+++ src/main/java/Tetriminos/Tetriminos.java
@@ -1,26 +1,98 @@
 package Tetriminos;
 
-abstract class Tetriminos {
-    public enum Shape {
-        I, J, L, O, S, T, Z
+import java.util.HashMap;
+
+public abstract class Tetriminos {
+
+    public enum Type {
+        Mino_I, 
+        Mino_J, 
+        Mino_L, 
+        Mino_O, 
+        Mino_S, 
+        Mino_T, 
+        Mino_Z
     }
 
-    Shape shape;
+    protected Type type;
 
     public enum Orientation {
-        Up, Right, Down, Left
+        Up(0), 
+        Right(1), 
+        Down(2), 
+        Left(3);
+
+        int mIndex;
+
+        Orientation(int i) {
+            mIndex = i;
+        }
+
+        int index() {
+            return mIndex;
+        }
+
+        static Orientation getOrientation(int i) {
+            switch (i) {
+                case 0:
+                    return Up;
+                case 1:
+                    return Right;
+                case 2:
+                    return Down;
+                case 3:
+                    return Left;
+                default:
+                    return Up;
+            }
+        }
     }
 
-    Orientation orientation;
+    protected Orientation orientation;
 
-    public abstract void rotateClockwise();
-    public abstract void rotateAntiClockwise();
+    protected int[] position = new int[2]; // {x, y}
 
-    public abstract void moveLeft();
-    public abstract void moveRight();
+    protected HashMap<Orientation, Byte[][]> shape;
 
-    public abstract void softDrop();
-    public abstract void hardDrop();
+    public Tetriminos(int[] position) {
+        this.position = position;
+        this.orientation = Orientation.Up;
+    }
 
-    public abstract void place();
+    public Byte[][] getShape() {
+        return shape.get(orientation);
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public void rotateClockwise() {
+        int o = orientation.index();
+        o = (o + 1) % 4;
+        this.orientation = Orientation.getOrientation(o);
+    }
+
+    public void rotateAntiClockwise() {
+        int o = orientation.index();
+        o = (o - 1) % 4;
+        if (o < 0) {
+            o += 4;
+        }
+        this.orientation = Orientation.getOrientation(o);
+    }
+
+    public void moveLeft() {
+        --position[0];
+    }
+    public void moveRight() {
+        ++position[0];
+    }
+
+    public void softDrop() {
+        ++position[1];
+    }
+    public void hardDrop(int y) {
+        position[1] = y;
+    }
 }
