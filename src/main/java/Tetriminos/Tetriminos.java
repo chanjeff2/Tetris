4a9ff5c82ab83ad7d54734165350fa169b1dc404
chanjeff2 <jeffchan142@gmail.com>
2021-01-19 18:34:07 +0800

    first-commit

---
+++ src/main/java/Tetriminos/Tetriminos.java
@@ -0,0 +1,8 @@
+package Tetriminos;
+
+abstract class Tetriminos {
+    public abstract void rotateClockwise();
+    public abstract void rotateAntiClockwise();
+    public abstract void drop();
+    public abstract void place();
+}
