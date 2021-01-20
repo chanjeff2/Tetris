609ac799481d87420479a6d837880714aad67d98
CurlyRhapsody <matthewchan60792@gmail.com>
2021-01-20 15:58:23 +0800

    Added all Tetromino files

---
+++ src/main/java/Tetriminos/Mino_L.java
@@ -0,0 +1,53 @@
+package Tetriminos;
+
+import java.util.HashMap;
+
+public class Mino_L extends Tetriminos {
+    public Mino_L(int[] position) {
+        super(position);
+
+        this.type = Type.Mino_L;
+
+        this.shape = new HashMap<Orientation, Byte[][]>();
+
+        this.shape.put(
+            Orientation.Up, 
+            new Byte[][] 
+            {
+                {0, 0, 1}
+                {1, 1, 1},
+                {0, 0, 0}
+            }
+        );
+
+        this.shape.put(
+            Orientation.Right, 
+            new Byte[][] 
+            {
+                {0, 1, 0}
+                {0, 1, 0},
+                {0, 1, 1}
+            }
+        );
+
+        this.shape.put(
+            Orientation.Down, 
+            new Byte[][] 
+            {
+                {0, 0, 0}
+                {1, 1, 1},
+                {1, 0, 0}
+            }
+        );
+
+        this.shape.put(
+            Orientation.Left, 
+            new Byte[][] 
+            {
+                {1, 1, 0}
+                {0, 1, 0},
+                {0, 1, 0}
+            }
+        );
+    }
+}
